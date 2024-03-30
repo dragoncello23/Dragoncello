c010976137624e251778a45c66605be8478f5a04
dragoncello23 <129548149+dragoncello23@users.noreply.github.com>
2024-03-30 18:10:53 +0100

    Versione 1.0.0

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function redirectToPage(pageURL) {
     window.location.href = pageURL;
 }
+document.getElementById("copy-button").addEventListener("click", function() {
+    var iframeText = document.getElementById("my-iframe").outerHTML;
+    copyTextToClipboard(iframeText);
+});
+
+function copyTextToClipboard(text) {
+    navigator.clipboard.writeText(text).then(function() {
+        var copyStatus = document.getElementById("copy-status");
+        copyStatus.style.display = "block";
+        setTimeout(function() {
+            copyStatus.style.display = "none";
+        }, 5000); // Nasconde la notifica dopo 5 secondi
+    });
+}
